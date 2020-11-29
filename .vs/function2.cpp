25c04ff4b1def9456913edcc49a51fc4e1ea0e9d
Himanshu Rai <himanshurairai560@gmail.com>
2020-11-29 23:50:29 +0300

    fuction example added

---
+++ .vs/function2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+void printInt(int printable){
+std::cout << "The int you passed in has value " << printable << std::endl;
+}
+/* void foo( int &i )
+{
+++i;
+cout<<i<<endl;
+} */
+
+/* void printIntArr(int *array_arg, int array_len){
+std::cout << "The length of the array is " << array_len << std::endl;
+for(int i = 0; i < array_len; i++)
+std::cout << "Array[" << i << "] = " << array_arg[i] << std::endl; } */
+
+
+int main()
+{
+
+    int my_array[5];
+    for(int i = 0; i < 5; i++){
+        my_array[i] = i * 2;
+        }
+    for(int i = 0; i < 5; i++)
+        printInt(my_array[i]); // <-- We pass in a dereferenced array element
+        
+   // printIntArr(my_array, 5);
+         
+/* int bar = 5; // bar == 5
+foo( bar ); // bar == 6
+foo( bar ); // bar == 7 */
+return 0;
+}
+
---
+++ .vs/swtich.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+//#define deb(x) cout<<#x<<"="<<x<<endl;
+
+int main(){
+   // int a=6;
+   char ch=0;
+    //deb(a);
+   cout<<""
+    return 0;
+}
