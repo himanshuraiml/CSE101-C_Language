f882e5a9e9a207055688dcb6f61a70dd73d913bd
Himanshu Rai <himanshurairai560@gmail.com>
2020-11-29 15:23:06 +0300

    c++ examples

---
+++ loopex1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    //for (int i=0;i<5;i++) {
+      //  cout<<"Hello World ! \n";
+      int i=0;
+      for (;i<5;) {
+          cout<<"Hello World ! \n";
+          i++;
+      }
+    //}
+
+}
---
+++ whileex.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int i=0;
+    bool condition =true;
+    while (condition)
+    {
+        cout<<"Hello World! \n";
+        i++;
+        if(!(i<5))  //Note : No curly brackets used 
+        condition=false; 
+    }  
+
+    do {
+        cout<<"This World is beautifl";
+        i++;
+    }
+    while (i<5);
+   
+    
+
+}
